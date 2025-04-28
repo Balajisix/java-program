a89997d887110cef7ca393b0bbbc356f0bfbf158
Balaji <balajivs0305@gmail.com>
2025-04-28 22:22:30 +0530

    updates

---
+++ src/ArraylistExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArraylistExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        System.out.println(arr.toString());
+    }
+}
